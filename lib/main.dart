d5c8015ae00c231c01edd15e501214cdf5e04528
Iancu Jianu <iancujianu98@gmail.com>
2025-03-23 16:58:06 +0200

    my solution to adding quiz & qs stateful widget classes

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:quizz_app/start_screen.dart';
+import 'package:quizz_app/quiz.dart';
 
 void main() {
     runApp(
-        MaterialApp(
-            home: Scaffold(
-                body: Container(
-                    decoration: const BoxDecoration(
-                        gradient: LinearGradient(
-                            colors: [
-                                Colors.deepOrange,
-                                Colors.greenAccent
-                            ],
-                            begin: Alignment.topLeft,
-                            end: Alignment.bottomRight
-                        )
-                    ),
-                    child: const StartScreen()
-                )
-            )
-        )
+        Quiz()
     );
 }
---
+++ lib/quiz.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:quizz_app/start_screen.dart';
+
+class Quiz extends StatefulWidget {
+    const Quiz({super.key});
+
+    @override
+    State<StatefulWidget> createState() {
+        return _QuizState();
+    }
+
+}
+
+class _QuizState extends State<Quiz> {
+
+    @override
+    Widget build(BuildContext context) {
+        return MaterialApp(
+            home: Scaffold(
+                body: Container(
+                    decoration: const BoxDecoration(
+                        gradient: LinearGradient(
+                            colors: [
+                                Colors.deepOrange,
+                                Colors.greenAccent
+                            ],
+                            begin: Alignment.topLeft,
+                            end: Alignment.bottomRight
+                        )
+                    ),
+                    child: const StartScreen()
+                )
+            )
+        );
+    }
+}
